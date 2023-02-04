a299846e113bd8b9f2a0c736d8059e1e00c8acbc
Samyog Bohara <fallendemon174@gmail.com>
2023-02-04 10:28:07 +0545

    added word limit to subtitle

--- lib/screen/bottom_screen/homescreen.dart
+++ lib/screen/bottom_screen/homescreen.dart
@@ -21,47 +21,67 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder(
-            future: _blogs,
-            builder:
-                (BuildContext context, AsyncSnapshot<List<Blog>> snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    Blog blog = snapshot.data![index];
-                    return Container(
-                      color: Color.fromARGB(255, 221, 221, 221),
-                      padding: const EdgeInsets.all(10),
-                      child: Card(
-                        child: ListTile(
-                          title: Padding(
-                            padding: const EdgeInsets.only(bottom: 15),
-                            child: Text(
-                              blog.title,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
+      body: Container(
+        color: Color.fromARGB(255, 221, 221, 221),
+        child: FutureBuilder(
+          future: _blogs,
+          builder: (BuildContext context, AsyncSnapshot<List<Blog>> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Blog blog = snapshot.data![index];
+
+                  int wordCount = 20;
+                  List<String> words = blog.content.split(" ");
+                  String limitedContent = words.take(wordCount).join(" ");
+                  if (words.length > wordCount) {
+                    limitedContent += "...";
+                  }
+
+                  return Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        title: Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Text(
+                            blog.title,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          subtitle: Text(blog.content),
-                          leading: const Icon(Icons.library_books),
-                          // onTap: () {},
                         ),
+                        subtitle: Text(
+                          limitedContent,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        leading: const Icon(
+                          Icons.library_books,
+                          size: 30,
+                        ),
+                        // onTap: () {},
                       ),
-                    );
-                  },
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('An error occurred: ${snapshot.error}'),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }));
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('An error occurred: ${snapshot.error}'),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
