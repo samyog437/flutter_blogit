7defbf057120aa50f584270c4b558ef0b91188b2
Samyog Bohara <fallendemon174@gmail.com>
2023-02-15 12:29:02 +0545

    remote dashboard for wearos

--- lib/screen/wearos/wear_dashboard.dart
+++ lib/screen/wearos/wear_dashboard.dart
@@ -1,3 +1,6 @@
+import 'package:blogit/model/blog.dart';
+import 'package:blogit/repository/blog_respository.dart';
+import 'package:blogit/screen/blogdetail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,6 +16,14 @@ class WearDashboardScreen extends StatefulWidget {
 }
 
 class _WearDashboardScreenState extends State<WearDashboardScreen> {
+  late Future<List<Blog>> _blogs;
+
+  @override
+  void initState() {
+    super.initState();
+    _blogs = BlogRepositoryImpl().getAllBlog();
+  }
+
   @override
   Widget build(BuildContext context) {
     return WatchShape(
@@ -20,11 +31,95 @@ class _WearDashboardScreenState extends State<WearDashboardScreen> {
         return AmbientMode(
           builder: ((context, mode, child) {
             return Scaffold(
-              appBar: AppBar(
-                title: const Text('Login'),
-              ),
               body: Container(
-                child: const Text('Dashboard'),
+                color: const Color(0xFFad5389),
+                child: FutureBuilder(
+                  future: _blogs,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Blog>> snapshot) {
+                    if (snapshot.hasData) {
+                      return Center(
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: Center(
+                                  child: Text(
+                                    'Blogs',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: snapshot.data!.length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    Blog blog = snapshot.data![index];
+
+                                    int wordCount = 20;
+                                    List<String> words =
+                                        blog.content.split(" ");
+                                    String limitedContent =
+                                        words.take(wordCount).join(" ");
+                                    if (words.length > wordCount) {
+                                      limitedContent += "...";
+                                    }
+
+                                    return Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 5, vertical: 2),
+                                      child: Card(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        child: ListTile(
+                                          onTap: () {
+                                            Navigator.pushNamed(
+                                                context, BlogDetailScreen.route,
+                                                arguments: blog);
+                                          },
+                                          title: Padding(
+                                            padding: const EdgeInsets.only(
+                                                bottom: 15),
+                                            child: Text(
+                                              blog.title,
+                                              style: const TextStyle(
+                                                fontSize: 10,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                            limitedContent,
+                                            style:
+                                                const TextStyle(fontSize: 10),
+                                          ),
+                                          // onTap: () {},
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text('An error occurred: ${snapshot.error}'),
+                      );
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
               ),
             );
           }),
