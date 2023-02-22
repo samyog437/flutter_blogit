d1da7f5e13b7448f7f969eca2d4c116e21ce271c
Samyog Bohara <fallendemon174@gmail.com>
2023-02-22 17:54:53 +0545

    added create blog remote

--- lib/data_source/remote_data_source/blog_data_source.dart
+++ lib/data_source/remote_data_source/blog_data_source.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:blogit/app/constants.dart';
 import 'package:blogit/data_source/remote_data_source/response/blog_response.dart';
 import 'package:blogit/helper/http_service.dart';
 import 'package:blogit/model/blog.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http_parser/http_parser.dart';
+import 'package:mime/mime.dart';
 
 class BlogRemoteDataSource {
   final Dio _httpServices = HttpServices().getDioInstance();
@@ -50,6 +54,43 @@ class BlogRemoteDataSource {
     } catch (e) {
       print("Error: $e");
       throw Exception('Failed to load a Blog');
+    }
+  }
+
+  Future<int> createBlog(File? file, Blog blog) async {
+    try {
+      MultipartFile? image;
+      if (file != null) {
+        var mimeType = lookupMimeType(file.path);
+        image = await MultipartFile.fromFile(
+          file.path,
+          filename: file.path.split('/').last,
+          contentType: MediaType('image', mimeType!.split("/")[1]),
+        );
+      }
+
+      FormData formData = FormData.fromMap({
+        'image': image,
+        'title': blog.title,
+        'content': blog.content,
+      });
+
+      Response response = await _httpServices.post(
+        Constant.blogURL,
+        data: formData,
+      );
+      print("API endpoint: ${Constant.blogURL}");
+      print("Status code: ${response.statusCode}");
+      print("Response data: ${response.data}");
+      if (response.statusCode == 201) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } catch (e) {
+      print("Error: $e");
+      throw Exception('Failed to create Blog');
+      // return 0;
     }
   }
 }
--- lib/repository/blog_respository.dart
+++ lib/repository/blog_respository.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:blogit/data_source/remote_data_source/blog_data_source.dart';
 import 'package:blogit/model/blog.dart';
 
 abstract class BlogRepository {
   Future<List<Blog>> getAllBlog();
   Future<Blog?> getABlog(id);
+  Future<int> createBlog(File? file, Blog blog);
 }
 
 class BlogRepositoryImpl extends BlogRepository {
@@ -15,5 +18,10 @@ class BlogRepositoryImpl extends BlogRepository {
   @override
   Future<Blog?> getABlog(id) {
     return BlogRemoteDataSource().getABlog(id);
+  }
+
+  @override
+  Future<int> createBlog(File? file, Blog blog) async {
+    return BlogRemoteDataSource().createBlog(file, blog);
   }
 }
