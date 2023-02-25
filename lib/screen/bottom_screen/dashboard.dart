bc93093d1986a976b13f3f557ac13eecb92db9b0
Samyog Bohara <fallendemon174@gmail.com>
2023-02-25 18:28:25 +0545

    added logout

--- lib/screen/bottom_screen/dashboard.dart
+++ lib/screen/bottom_screen/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:blogit/screen/bottom_screen/addblogscreen.dart';
 import 'package:blogit/screen/bottom_screen/homescreen.dart';
 import 'package:blogit/screen/bottom_screen/profilescreen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -20,12 +21,28 @@ class _DashboardScreenState extends State<DashboardScreen> {
     const AddBlogScreen(),
     const ProfileScreen(),
   ];
+
+  _clearSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('username');
+    await prefs.remove('password');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: const Key('dashboardPage'),
       appBar: AppBar(
         title: const Text('Dashboard'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () async {
+                _clearSharedPreferences();
+                Navigator.pushReplacementNamed(context, '/loginScreen');
+              })
+        ],
+        automaticallyImplyLeading: false,
       ),
       body: lstDasboardScreen[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
